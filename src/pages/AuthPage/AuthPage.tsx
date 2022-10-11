bbe0f8985a72f2b046b8b99ddf999ffa44f5ccc9
kien <reckpo123@gmail.com>
2022-10-11 13:59:55 +0700

    edit auth page

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -6,17 +6,20 @@ import Login from "components/Login/Login";
 function AuthPage() {
 
     return (
-        <div className="auth-page">
-            <div className="auth-content">
-                <Tabs defaultActiveKey="1">
-                    <Tabs.TabPane tab="Đăng ký" key="1">
-                        <Register />
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="Đăng nhập" key="2">
-                        <Login />
-                    </Tabs.TabPane>
-                </Tabs>
+        <div className="auth">
+            <div className="auth-page">
+                <div className="auth-content">
+                    <Tabs defaultActiveKey="1">
+                        <Tabs.TabPane tab="Đăng ký" key="1">
+                            <Register />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="Đăng nhập" key="2">
+                            <Login />
+                        </Tabs.TabPane>
+                    </Tabs>
+                </div>
             </div>
+            <img className="bg-right" src={require("assets/images/background/right-bg.png")} alt="" />
         </div>
     );
 }
