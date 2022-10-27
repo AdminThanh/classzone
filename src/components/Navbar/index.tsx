f6450e95a88e0237c1e98a6d8384856875f74366
Đào Đức Minh Khôi <daoducminhkhoi@gmail.com>
2022-10-27 22:09:05 +0700

    Update navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,9 @@
+import { useQuery } from '@apollo/client';
+import logo from 'assets/images/logo.png';
+import { useAuth } from 'contexts/AuthContext';
+import { getAllUser } from 'graphql/user';
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { INavItem } from 'routes/navs';
 import {
@@ -6,16 +11,10 @@ import {
   ExitIcon,
   ProfileIcon,
   SettingIcon,
+  ShowmoreIocn,
 } from 'utils/drawer';
-import logo from 'assets/images/logo.png';
-import './Navbar.scss';
-import { useLazyQuery, useMutation, useQuery } from '@apollo/client';
-import { LogoutDocument } from 'gql/graphql';
 import JWTManager from 'utils/jwt';
-import { notification } from 'antd';
-import { useTranslation } from 'react-i18next';
-import { useAuth } from 'contexts/AuthContext';
-import { getAllUser } from 'graphql/user';
+import './Navbar.scss';
 
 interface IUserControl extends INavItem {
   onClick?: () => void;
@@ -26,9 +25,11 @@ export interface INavbarProps {
 
 const Navbar = (props: INavbarProps) => {
   const { navList = [] } = props;
-  const { isAuthenticated, logout } = useAuth();
+  const { isAuthenticated, logout, auth } = useAuth();
   const { refetch } = useQuery(getAllUser);
   const { t } = useTranslation();
+
+  console.log('user', auth);
 
   const listUserControl: IUserControl[] = useMemo(() => {
     return [
@@ -64,54 +65,102 @@ const Navbar = (props: INavbarProps) => {
   }, []);
 
   return (
-    <div className="navbar">
-      <div className="navbar__skin">
-        <Link to="/" className="navbar__logo">
-          <img src={logo} />
-        </Link>
-        <div className="navbar__skeleton">
-          <ul className="navbar__listItem">
+    <>
+      <div className="navbar">
+        <div className="navbar__skin">
+          <Link to="/" className="navbar__logo">
+            <img src={logo} alt="logo" />
+          </Link>
+          <div className="navbar__skeleton">
+            <ul className="navbar__listItem">
+              {navList?.map((item, idx) => (
+                <li key={idx} className="navbar__item">
+                  <Link to={item.path} className="navbar__link">
+                    <item.icon className="navbar__icon" />
+                    <span className="navbar__label">
+                      {t(`navbar.${item.label}`)}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="navbar__showmore">
+              <ShowmoreIocn />
+            </div>
+
+            {/* User control */}
+            {isAuthenticated && (
+              <div className="navbar__user">
+                <div className="navbar__label">Chào, {auth.username}</div>
+                <DefaultAvatar className="navbar__avatar" />
+
+                {/* dropdown */}
+                <div className="navbar__dropdown">
+                  <div className="navbar__listControl">
+                    {listUserControl.map((item, idx) => (
+                      <Link
+                        onClick={item.onClick}
+                        key={idx}
+                        to=""
+                        className="navbar__controlItem"
+                      >
+                        <item.icon className="navbar__control-icon"></item.icon>
+                        <span className="navbar__control-label">
+                          {t(`user_control.${item.label}`)}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* Responsive navbar */}
+      <div className="navbar__modal navbar__modal--active">
+        <div className="navbar__modal__skin">
+          <div className="navbar__modal__header">
+            <div className="navbar__modal__avatar">
+              <DefaultAvatar className="navbar__avatar" />
+            </div>
+            <div className="nav__modal_username">Chào, {auth?.username}</div>
+          </div>
+          <div className="navbar__modal__body">
             {navList?.map((item, idx) => (
-              <li key={idx} className="navbar__item">
-                <Link to={item.path} className="navbar__link">
-                  <item.icon className="navbar__icon" />
-                  <span className="navbar__label">
+              <div key={idx} className="navbar__modal__item ">
+                <Link to={item.path} className="navbar__modal__link">
+                  <item.icon className="navbar__modal__icon" />
+                  <span className="navbar__modal__label">
                     {t(`navbar.${item.label}`)}
                   </span>
                 </Link>
-              </li>
-            ))}
-          </ul>
-
-          {/* User control */}
-          {isAuthenticated && (
-            <div className="navbar__user">
-              <div className="navbar__label">Chào, Hồ Thị Bích Ngọc</div>
-              <DefaultAvatar className="navbar__avatar" />
-
-              {/* dropdown */}
-              <div className="navbar__dropdown">
-                <div className="navbar__listControl">
-                  {listUserControl.map((item, idx) => (
-                    <Link
-                      onClick={item.onClick}
-                      key={idx}
-                      to=""
-                      className="navbar__controlItem"
-                    >
-                      <item.icon className="navbar__control-icon"></item.icon>
-                      <span className="navbar__control-label">
-                        {t(`user_control.${item.label}`)}
-                      </span>
-                    </Link>
-                  ))}
-                </div>
               </div>
+            ))}
+          </div>
+          <div className="navbar__modal__footer">
+            <div className="navbar__modal__footer__container">
+              {listUserControl.map((item, idx) => (
+                <div className="navbar__modal__item">
+                  <Link
+                    onClick={item.onClick}
+                    key={idx}
+                    to=""
+                    className="navbar__modal__link"
+                  >
+                    <item.icon className="navbar__modal__icon"></item.icon>
+                    <span className="navbar__control__label">
+                      {t(`user_control.${item.label}`)}
+                    </span>
+                  </Link>
+                </div>
+              ))}
             </div>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/utils/drawer.ts
+++ src/utils/drawer.ts
@@ -15,4 +15,4 @@ export { ReactComponent as EditIcon } from 'assets/images/edit-icon.svg';
 export { ReactComponent as CancelIcon } from 'assets/images/cancel-icon.svg';
 export { ReactComponent as SaveIcon } from 'assets/images/save-icon.svg';
 export { ReactComponent as BinIcon } from 'assets/images/bin-icon.svg';
-
+export { ReactComponent as ShowmoreIocn } from 'assets/images/icons/showmore.svg';
