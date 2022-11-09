25a277b36f82b31527c811b67e20ea7d6fe201b1
ThanhZOne <thanhboi312@.com>
2022-11-09 17:08:38 +0700

    upload done

--- src/pages/CreateQuestion/index.tsx
+++ src/pages/CreateQuestion/index.tsx
@@ -1,220 +1,135 @@
 import './CreateQuestion.scss';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
-import { Avatar, Button, Form, Input } from 'antd';
-import { useMemo, useState } from 'react';
-import BreadCrumb from 'components/BreadCrumb';
-import FilterTags, { IOptionTag } from 'components/FilterTags';
-import { useTranslation } from 'react-i18next';
-import { Editor } from '@tinymce/tinymce-react';
-import { Checkbox, InputNumber } from 'antd';
+import { CheckCircleOutlined, DeleteOutlined, MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { Avatar, Button, Checkbox, Form, Input, InputNumber, Radio, Space } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
+import { Editor } from '@tinymce/tinymce-react';
+import BreadCrumb from 'components/BreadCrumb';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import FilterTags, { IOptionTag } from 'components/FilterTags';
 
-const CreateQuestion = () => {
-  const { t } = useTranslation();
+const CreateQuession = () => {
+    const editorRef = useRef(null);
+    const [isCheck, setIsCheck] = useState(true);
 
-  const [text, setText] = useState('');
-  const tagOpts: IOptionTag[] = useMemo(
-    () => [
-      {
-        label: 'HTML',
-        value: '1',
-      },
-      {
-        label: 'CSS',
-        value: '2',
-      },
-      {
-        label: 'ReactJS',
-        value: '3',
-      },
-    ],
-    []
-  );
-
-  const handleChangeFilterTags = (value: string[]) => {
-    console.log('Change', value);
-  };
-
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 12 },
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 12 },
-    },
-  };
-
-  const onFinish = (values: any) => {
-    console.log('Received values of form:', values);
-  };
-
-  return (
-    <div className="site_wrapper">
-      <div className="site_container">
-        <BreadCrumb
-          routes={[
+    const onFinish = (values: any) => {
+        console.log('Payload:', values);
+        console.log('question:', values.question.level.content);
+        console.log(editorRef);
+    };
+    const tagOpts: IOptionTag[] = useMemo(
+        () => [
             {
-              name: t('navbar.home'),
-              path: '/',
+                label: 'HTML',
+                value: '1',
             },
             {
-              name: t('navbar.question_management'),
-              path: '/question',
+                label: 'CSS',
+                value: '2',
             },
             {
-              name: t('navbar.add_question'),
-              path: '/create_question',
+                label: 'ReactJS',
+                value: '3',
             },
-          ]}
-        />
+        ],
+        []
+    );
 
-        <div className="create_quession">
-          <div className="action-navbar">
-            <label>{t('my_quession.tags')}: </label>
-            <FilterTags
-              placeholder={t('my_quession.choose_tags')}
-              isShowTagControl
-              opts={tagOpts}
-              onChange={handleChangeFilterTags}
-            />
-          </div>
-          <div className="create_quession-content">
-            <div className="editor">
-              <label className="title_answer">Câu hỏi:</label>
-              <Editor
-                value={text}
-                onInit={(evt, editor) => {
-                  setText(editor.getContent({ format: 'text' }));
-                }}
-                onEditorChange={(newValue, editor) => {
-                  setText(newValue);
-                  setText(editor.getContent({ format: 'text' }));
-                }}
-                init={{
-                  height: 400,
-                  plugins: [
-                    'advlist',
-                    'autolink',
-                    'lists',
-                    'link',
-                    'image',
-                    'charmap',
-                    'preview',
-                    'anchor',
-                    'searchreplace',
-                    'visualblocks',
-                    'code',
-                    'fullscreen',
-                    'insertdatetime',
-                    'media',
-                    'table',
-                    'help',
-                    'wordcount',
-                  ],
-                  toolbar:
-                    'undo redo | blocks | ' +
-                    'bold italic backcolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                }}
-              />
-              {/* <pre>{text}</pre> */}
-            </div>
-            <div className="choose_answer">
-              <Checkbox>Chọn nhiều đáp án</Checkbox>
-              <div className="point mt-2">
-                <label htmlFor="">Điểm:</label>
-                <InputNumber min={0} max={100} defaultValue={3} />
-              </div>
-              <div className="answers mt-2">
-                <label htmlFor="" className="title_answer">
-                  Đáp án:
-                </label>
+    const handleChangeFilterTags = (value: string[]) => {
+        console.log('Change', value);
+    };
+    const handleChangeIsCheck = () => {
+        setIsCheck(!isCheck);
+        console.log(isCheck);
+    }
 
-                <Form name="dynamic_form_item" onFinish={onFinish}>
-                  <Form.List
-                    name="names"
-                    rules={[
-                      {
-                        validator: async (_, names) => {
-                          if (!names || names.length < 2) {
-                            return Promise.reject(
-                              new Error('At least 2 passengers')
-                            );
-                          }
-                        },
-                      },
-                    ]}
-                  >
-                    {(fields, { add, remove }, { errors }) => (
-                      <>
-                        {fields.map((field, index) => (
-                          <Form.Item
-                            {...formItemLayout}
-                            required={false}
-                            key={field.key}
-                          >
-                            <Form.Item
-                              {...field}
-                              validateTrigger={['onChange', 'onBlur']}
-                              rules={[
-                                {
-                                  required: true,
-                                  whitespace: true,
-                                  message:
-                                    "Nội dung không được để trống",
-                                },
-                              ]}
-                              noStyle
-                            >
-                              <div className="quession_item">
-                                <Avatar className="quession_item-sentence">
-                                  1
-                                </Avatar>
-                                <TextArea
-                                  className="quession_item-content"
-                                  placeholder="Nhập câu hỏi"
-                                  autoSize
-                                />
-                              </div>
+
+    return (
+        <div className="createQuession">
+            <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
+                <div className='action-navbar'>
+                    <label>Tag</label>
+                    <FilterTags
+                        placeholder={'Tag'}
+                        isShowTagControl
+                        opts={tagOpts}
+                        onChange={handleChangeFilterTags}
+                    />
+                </div>
+
+                <Form.Item name={'point'} rules={[{ required: true, message: 'Không được để trống' }]} label="Điểm" >
+                    <InputNumber min={0} />
+                </Form.Item>
+
+                <Form.Item name={'question'} rules={[{ required: true, message: 'Không được để trống' }]} label="Câu hỏi" className="quession">
+                    <Editor
+                        apiKey='7ad8eq7icu3n630t4u3ioq10q46dh4k70mpveov40xv5ofo9'
+                        onInit={(evt: any, editor: any) => editorRef.current = editor}
+                        init={{
+                            height: 200,
+                            plugins: [
+                                'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export'
+                            ],
+                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                            menubar: 'file edit view insert format tools table tc help',
+                            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+                            autosave_ask_before_unload: true,
+                            autosave_interval: '30s',
+                        }}
+                    />
+                </Form.Item>
+
+                <Form.Item name="isCheck" valuePropName="checked">
+                    <Checkbox onClick={handleChangeIsCheck} >Cho phép chọn nhiều đáp án</Checkbox>
+                </Form.Item>
+
+                <Form.List name="answer">
+                    {(fields, { add, remove }) => (
+                        <>
+                            <label>Đáp án</label>
+                            {fields.map(({ key, name, ...restField }) => (
+                                <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                                    <Form.Item
+                                        className='check_result'
+                                        {...restField}
+                                        name={[name, 'check_result']}
+                                        {...restField}
+                                        valuePropName="checked"
+                                    >
+                                        <Checkbox className='hidden'>
+                                            {key + 1}
+                                        </Checkbox>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        {...restField}
+                                        name={[name, 'quession']}
+                                        {...restField}
+                                        rules={[{ required: true, message: 'Không được để trống' }]}
+                                    >
+                                        {/* <Checkbox className='hidden'>
+                                        </Checkbox>
+                                        <Avatar>{key}</Avatar> */}
+                                        <Input placeholder="Câu trả lời" />
+                                    </Form.Item>
+                                    <DeleteOutlined onClick={() => remove(name)} />
+                                </Space>
+                            ))}
+                            <Form.Item>
+                                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                                    Thêm câu hỏi
+                                </Button>
                             </Form.Item>
-                            {fields.length > 1 ? (
-                              <MinusCircleOutlined
-                                className="dynamic-delete-button"
-                                onClick={() => remove(field.name)}
-                              />
-                            ) : null}
-                          </Form.Item>
-                        ))}
-                        <Form.Item>
-                          <Button
-                          className='btn_add_quession-item'
-                            type="dashed"
-                            onClick={() => add()}
-                            style={{ width: '20%' }}
-                          >
-                            Thêm câu trả lời
-                          </Button>
-                          <Form.ErrorList errors={errors} />
-                        </Form.Item>
-                      </>
+                        </>
                     )}
-                  </Form.List>
+                </Form.List>
 
-                  <Form.Item>
+                <Form.Item className='btn_action'>
                     <Button type="primary" htmlType="submit">
-                      Submit
+                        Xuất bản
                     </Button>
-                  </Form.Item>
-                </Form>
-              </div>
-            </div>
-          </div>
+                </Form.Item>
+            </Form>
         </div>
-      </div>
-    </div>
-  );
-};
-export default CreateQuestion;
+    )
+}
+export default CreateQuession; 
